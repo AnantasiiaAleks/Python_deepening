c342fa64fe60229e21db69ede73bf88c0f1963e7
AnastasiiaAleks <chiffka@gmail.com>
2024-10-20 18:11:10 +0300

    task_6 каждое слово текста с новой строки и пронумеровано

--- Sem_3/word_in_new_string.py
+++ Sem_3/word_in_new_string.py
@@ -2,8 +2,8 @@
 Пользователь вводит строку текста. Вывести каждое слово с новой строки.
 ✔ Строки нумеруются начиная с единицы.
 ✔ Слова выводятся отсортированными согласно кодировки Unicode.
-✔ Текст выравнивается по правому краю так, чтобы у самого длинного
-слова был один пробел между ним и номером строки.
+✔ Текст выравнивается по правому краю так, чтобы у самого длинного слова был
+один пробел между ним и номером строки.
 '''
 
 text = input('Введите текст: ')
@@ -13,10 +13,11 @@ text_without_chars = (text.replace(',', '')
                       .replace('.', '')
                       .replace(' -', ''))
 list_text = sorted(text_without_chars.split())
+print(list_text)
 max_len = 0
 for word in list_text:
     word_len = len(word)
     if word_len > max_len:
         max_len = word_len
 for num, word in enumerate(list_text, 1):
-    print(f'{num} {word: >{max_len}}')
+    print(f'{num:>2} {word: >{max_len}}')
