81f6a51fecf6c175954b62637aae71c49b8c2de0
Anastasiia <chiffka@gmail.com>
2025-05-04 18:47:36 +0300

    пересобран модуль проверки даты для запуска из терминала. вписана документация

--- Sem_6/moduls/date_modul.py
+++ Sem_6/moduls/date_modul.py
@@ -1,49 +1,62 @@
 '''
-Создайте модуль и напишите в нём функцию, которая
-получает на вход дату в формате DD.MM.YYYY
-� Функция возвращает истину, если дата может существовать
-или ложь, если такая дата невозможна.
-� Для простоты договоримся, что год может быть в диапазоне
-[1, 9999].
-� Весь период (1 января 1 года - 31 декабря 9999 года)
-действует Григорианский календарь.
-� Проверку года на високосность вынести в отдельную
-защищённую функцию.
+Создайте модуль и напишите в нём функцию, которая получает на вход дату в формате DD.MM.YYYY
+� Функция возвращает истину, если дата может существовать или ложь, если такая дата невозможна.
+� Для простоты договоримся, что год может быть в диапазоне [1, 9999].
+� Весь период (1 января 1 года - 31 декабря 9999 года) действует Григорианский календарь.
+� Проверку года на високосность вынести в отдельную защищённую функцию.
+В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
 '''
+from sys import argv
+
 _month_31 = (1, 3, 5, 7, 8, 10, 12)
 _month_30 = (4, 6, 9, 11)
-_month_feb = 2
+
 
 
 __all__ = ['date_check']
 
 def date_check(input_date: str) -> bool:
-    day, month, year = map(int, input_date.split('.'))
-    if 1 <= year <= 9999:
-        if month in _month_31 and 1 <= day <= 31:
-            return True
-        elif month in _month_30 and 1 <= day <= 30:
-            return True
-        elif month == 2:
-            if _is_leap(year) and 1 <= day <= 29:
-                return True
-            elif 1 <= day <= 28:
-                return True
+    '''
+    Проверяет валидность введенной даты по Григорианскому календарю
+    >>> python date_modul.py 29.02.2024
+        True
+    >>> python date_modul.py 31.06.2025
+        False
+    '''
+    try:
+        day, month, year = map(int, input_date.split('.'))
+    except ValueError:
+        return False    # Если введен некорректный формат даты
+
+    if not (1 <= year <= 9999):
+        return False
+    if not (1 <= month <= 12):
+        return False
+
+    if month in _month_31:
+        return  1 <= day <= 31
+    elif month in _month_30:
+        return 1 <= day <= 30
+    elif month == 2:
+        if _is_leap(year):
+            return 1 <= day <= 29
+        else:
+            return 1 <= day <= 28
+
     return False
 
 
 def _is_leap(year):
-    if (year % 4) == 0:
-        if (year % 100) == 0:
-            if (year % 400) == 0:
-                return True
-        else:
-            return False
-    else:
-        return True
-    return False
+    """
+    По Григорианскому календарю: если год делится на 400
+    или делится на 4, но не на 100, год считается високосным
+    """
+    return (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0))
 
 
 if __name__ == '__main__':
-    date_input = input('Введите дату строкой вида DD.MM.YYYY: ')
-    print(date_check(date_input))
+    if len(argv) != 2:
+        print('Использование: python date_modul.py DD:MM:YYY')
+    else:
+        date_input = argv[1]
+        print(date_check(date_input))
